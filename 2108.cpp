08bd16b39f774e394da5e5b649c45571ab541bee
kasak Patel <kasakpatel967@gmail.com>
2026-08-22 00:27:09 +0530

    Add files via upload

---
+++ 2108.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+
+        for (string word : words) {
+
+            int left = 0;
+            int right = word.length() - 1;
+
+            while (left < right) {
+
+                if (word[left] != word[right]) {
+                    break;
+                }
+
+                left++;
+                right--;
+            }
+
+            if (left >= right) {
+                return word;
+            }
+        }
+
+        return "";
+    }
+};
