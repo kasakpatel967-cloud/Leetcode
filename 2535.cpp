029972e512a5ebb4a3f85bbd65c43f5c0ce856b1
kasak Patel <kasakpatel967@gmail.com>
2026-08-24 09:38:52 +0530

    Add files via upload

---
+++ 2535.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int differenceOfSum(vector<int>& nums) {
+
+        int elementSum = 0;
+        int digitSum = 0;
+
+        for (int num : nums) {
+
+            elementSum += num;
+
+            while (num > 0) {
+                digitSum += num % 10;
+                num = num / 10;
+            }
+        }
+
+        return abs(elementSum - digitSum);
+    }
+};
