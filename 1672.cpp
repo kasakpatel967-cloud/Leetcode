f267c68755381dd73329ae8b0921affe3605e7f7
kasak Patel <kasakpatel967@gmail.com>
2026-09-09 01:14:05 +0530

    Add files via upload

---
+++ 1672.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int richest = 0;
+
+        for (int i = 0; i < accounts.size(); i++) {
+            int sum = 0;
+
+            for (int j = 0; j < accounts[i].size(); j++) {
+                sum = sum + accounts[i][j];
+            }
+
+            richest = max(richest, sum);
+        }
+
+        return richest;
+    }
+};
